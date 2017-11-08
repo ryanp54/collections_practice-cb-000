351639a0d7ab6e4d8c358aa8c0bbbd44a96568f5
Ryan Pfeifer <pfeifer.ryan@gmail.com>
2017-11-08 03:27:55 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -36,5 +36,5 @@ def sum_array(array)
 end
 
 def add_s(array)
-  array.each_with_index.collect { |item, index| item if index != 1 }
+  array.each_with_index.collect { |item, index| item << "s" if index != 1 }
 end
