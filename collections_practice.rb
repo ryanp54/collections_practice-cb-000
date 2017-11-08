bde8fd7233e3f9873e80523d909e8f3a6b6c558c
Ryan Pfeifer <pfeifer.ryan@gmail.com>
2017-11-08 03:05:50 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -2,5 +2,8 @@ def sort_array_asc(array)
   array.sort{ |a,b| a <=> b }
 end
 def sort_array_desc(array)
-  array.sort{ |a,b| -1 * (a <=> b) }
+  array.sort{ |a,b| b <=> a }
 end
+
+def sort_array_char_count(array)
+  array.sort{ |a, b| a.size <=> b.size }
