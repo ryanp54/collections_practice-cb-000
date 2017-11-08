1472be5989a0a67aee2168cd3f6f4f487e25397c
Ryan Pfeifer <pfeifer.ryan@gmail.com>
2017-11-08 03:28:47 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,40 @@
+def sort_array_asc(array)
+  array.sort{ |a,b| a <=> b }
+end
+def sort_array_desc(array)
+  array.sort{ |a,b| b <=> a }
+end
 
+def sort_array_char_count(array)
+  array.sort{ |a, b| a.size <=> b.size }
+end
+
+def kesha_maker(array)
+  array.collect do |item|
+    item[2] = "$"
+    item
+  end
+end
+
+def find_a(array)
+  array.select{ |item| item[0] == "a" }
+end
+
+def swap_elements(array)
+  swappee = array[1]
+  array[1] = array[2]
+  array[2] = swappee
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def sum_array(array)
+  array.inject(0) { |result, item| result + item }
+end
+
+def add_s(array)
+  array.each_with_index.collect { |item, index| index != 1 ? item << "s" : item }
+end
